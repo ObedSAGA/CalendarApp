6dbd04671081aedc6502709eca4431b8d29e5c4d
ObedSAGA <obedsg95@gmail.com>
2022-02-15 13:10:17 +0100

    Form validation and Moment format date

--- src/components/calendar/CalendarModal.js
+++ src/components/calendar/CalendarModal.js
@@ -1,10 +1,12 @@
 import Modal from "react-modal";
 import DateTimePicker from 'react-datetime-picker';
 import moment from "moment";
+import Swal from 'sweetalert2'
 
 import "../../styles.css";
 import { useState } from "react";
 
+// ESTILOS PROPIOS DE REACT MODAL
 const customStyles = {
   content: {
     top: "50%",
@@ -15,7 +17,6 @@ const customStyles = {
     transform: "translate(-50%, -50%)",
   },
 };
-
 Modal.setAppElement("#root");
 
 const now = moment().minutes(0).seconds(0).add(1, 'hours');
@@ -25,17 +26,63 @@ export const CalendarModal = () => {
 
   const [dateStart, setDateStart] = useState(now.toDate())
   const [dateEnd, setDateEnd] = useState(nowPlusOne.toDate())
+  const [titleValid, setTitleValid] = useState(true)
+
+  const [formValues, setFormValues] = useState({
+      title: 'Evento',
+      notes: '',
+      start: now.toDate(),
+      end: nowPlusOne.toDate(),
+  });
+
+  const {notes, title, start, end} = formValues;
+
+  const handleInputChange = ({target}) => {
+    setFormValues({
+      ...formValues,
+      [target.name]: target.value
+    });
+  }
+
 
   const closeModal = () => {
-
+    //TODO: CERRAR EL MODAL
   };
 
   const handleStartDate = (e) => {
     setDateStart(e);
+    setFormValues({
+      ...formValues,
+      start: e
+    })
   }
 
   const handleEndtDate = (e) => {
     setDateEnd(e)
+    setFormValues({
+      ...formValues,
+      end: e
+    })
+  }
+
+  const handleSubmitForm = (e) => {
+    e.preventDefault();
+    const momentStart = moment(start);
+    const momentEnd = moment(end);
+
+    if (momentStart.isSameOrAfter(momentEnd)) {
+      Swal.fire('Error', 'La fecha fin debe ser mayor a la fecha de inicio', 'error');
+      return;
+    }
+
+    if (title.trim().length < 2) {
+      return setTitleValid(false);
+    }
+
+    // TODO: realizar grabación en DB
+
+    setTitleValid(true);
+    closeModal();
   }
 
 
@@ -51,7 +98,10 @@ export const CalendarModal = () => {
     >
       <h1> Nuevo evento </h1>
       <hr />
-      <form className="container">
+      <form 
+          className="container"
+          onSubmit={handleSubmitForm}
+      >
         <div className="form-group">
           <label>Fecha y hora inicio</label>
           <DateTimePicker 
@@ -76,10 +126,12 @@ export const CalendarModal = () => {
           <label>Titulo y notas</label>
           <input
             type="text"
-            className="form-control"
+            className={`form-control ${!titleValid && 'is-invalid'}`}
             placeholder="Título del evento"
             name="title"
             autoComplete="off"
+            value={title}
+            onChange={handleInputChange}
           />
           <small id="emailHelp" className="form-text text-muted">
             Una descripción corta
@@ -93,6 +145,8 @@ export const CalendarModal = () => {
             placeholder="Notas"
             rows="5"
             name="notes"
+            value={notes}
+            onChange={handleInputChange}
           ></textarea>
           <small id="emailHelp" className="form-text text-muted">
             Información adicional
